770b57cd84c8e6d7e860087b543e88c52511620f
Drew Banin <drew@fishtownanalytics.com>
2018-09-11 22:07:19 -0400

    fix (some) integration tests

--- integration_tests/models/sql/test_union.sql
+++ integration_tests/models/sql/test_union.sql
@@ -1,16 +1,6 @@
 
-with unioned as (
-
-    select * from {{ dbt_utils.union_tables([
+{{ dbt_utils.union_tables([
         ref('data_union_table_1'),
         ref('data_union_table_2')]
-    ) }}
+) }} 
 
-)
-
-select
-    id,
-    name,
-    favorite_color
-
-from unioned
